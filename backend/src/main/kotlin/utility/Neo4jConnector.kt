a2799bb189f6808763bf5fc36763db2bcd925d17
Jakob Löhnertz <mail@jakob.codes>
2019-03-04 17:36:48 +0100

    Add the package with the relationships to the 'Neo4j' session factory

--- backend/src/main/kotlin/utility/Neo4jConnector.kt
+++ backend/src/main/kotlin/utility/Neo4jConnector.kt
@@ -6,11 +6,12 @@ import org.neo4j.ogm.session.SessionFactory
 
 
 const val Neo4jBoltConnectionUri = "bolt://localhost"
-const val ModelPackageIdentifier = "model.graph"
+const val NodeModelPackageIdentifier = "model.graph.node"
+const val RelationshipModelPackageIdentifier = "model.graph.relationship"
 
 object Neo4jConnector {
     private val configuration: Configuration = Configuration.Builder().uri(Neo4jBoltConnectionUri).build()!!
-    private val sessionFactory: SessionFactory = SessionFactory(configuration, ModelPackageIdentifier)
+    private val sessionFactory: SessionFactory = SessionFactory(configuration, NodeModelPackageIdentifier, RelationshipModelPackageIdentifier)
 
     fun saveEntity(entity: GraphModel) {
         val session = sessionFactory.openSession()
