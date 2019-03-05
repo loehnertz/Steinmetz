55dce21f253c2d8aa4ed7e89d90e3d3ca8f2e443
Jakob Löhnertz <mail@jakob.codes>
2019-03-05 14:40:16 +0100

    Implement more methods inside of the 'Neo4jConnector'

--- backend/src/main/kotlin/utility/Neo4jConnector.kt
+++ backend/src/main/kotlin/utility/Neo4jConnector.kt
@@ -1,7 +1,9 @@
 package utility
 
-import model.graph.GraphModel
+import model.graph.GraphEntity
 import org.neo4j.ogm.config.Configuration
+import org.neo4j.ogm.cypher.Filters
+import org.neo4j.ogm.session.Session
 import org.neo4j.ogm.session.SessionFactory
 
 
@@ -13,9 +15,26 @@ object Neo4jConnector {
     private val configuration: Configuration = Configuration.Builder().uri(Neo4jBoltConnectionUri).build()!!
     private val sessionFactory: SessionFactory = SessionFactory(configuration, NodeModelPackageIdentifier, RelationshipModelPackageIdentifier)
 
-    fun saveEntity(entity: GraphModel) {
-        val session = sessionFactory.openSession()
+    fun saveEntity(entity: GraphEntity) {
+        val session = createSession()
         session.save(entity)
         session.clear()
+    }
+
+    fun saveEntities(entities: Iterable<GraphEntity>) {
+        val session = createSession()
+        session.save(entities)
+        session.clear()
+    }
+
+    fun retrieveEntity(entityClass: Class<out GraphEntity>, filters: Filters): GraphEntity? {
+        val session = createSession()
+        val entity = session.loadAll(entityClass, filters).firstOrNull()
+        session.clear()
+        return entity
+    }
+
+    private fun createSession(): Session {
+        return sessionFactory.openSession()
     }
 }
