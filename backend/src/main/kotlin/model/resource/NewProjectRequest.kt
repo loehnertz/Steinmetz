94a80a08c333a5e247072e7a003d546ba0ad4606
Jakob Löhnertz <mail@jakob.codes>
2019-03-18 10:29:10 +0100

    Add a data class for incoming requests for new projects

---
+++ backend/src/main/kotlin/model/resource/NewProjectRequest.kt
@@ -0,0 +1,11 @@
+package model.resource
+
+import java.io.File
+
+
+data class NewProjectRequest(
+        val projectIdentifier: String,
+        val projectPlatform: String,
+        val staticAnalysisArchive: File,
+        val dynamicAnalysisArchive: File
+)
