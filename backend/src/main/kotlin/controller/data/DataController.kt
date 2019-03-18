69c87952bc7a24ddd2cbc5db7609803c1041554f
Jakob Löhnertz <mail@jakob.codes>
2019-03-18 10:28:35 +0100

    Add an endpoint controller for inserting new data

--- backend/src/main/kotlin/controller/data/DataController.kt
+++ backend/src/main/kotlin/controller/data/DataController.kt
@@ -1,16 +1,25 @@
 package controller.data
 
-import model.graph.node.Unit
+import controller.data.graph.GraphConverter
+import controller.data.graph.GraphInserter
+import model.graph.Graph
+import model.neo4j.node.Unit
+import model.resource.NewProjectRequest
 import org.neo4j.ogm.cypher.ComparisonOperator
 import org.neo4j.ogm.cypher.Filter
 import utility.Neo4jConnector
 
 
 class DataController {
+    fun insertProject(newProjectRequest: NewProjectRequest): Graph {
+        GraphInserter(newProjectRequest.projectIdentifier, newProjectRequest.projectPlatform, newProjectRequest.staticAnalysisArchive).insert()
+        return getGraph(newProjectRequest.projectIdentifier)
+    }
+
     fun getGraph(basePackageIdentifier: String): Graph {
         val filter = Filter("packageIdentifier", ComparisonOperator.STARTING_WITH, basePackageIdentifier)
         val units = Neo4jConnector.retrieveEntities(Unit::class.java, filter).map { it as Unit }
-        val relationships = GraphExtractor.convertUnitListToRelationships(units)
+        val relationships = GraphConverter.convertUnitListToRelationships(units)
         return Graph(relationships = relationships)
     }
 }
