b94c50c2176f992d69aa9c26ac8f287e588b90e4
Jakob Löhnertz <mail@jakob.codes>
2019-04-17 17:38:46 +0200

    Improve the inheritance structure inside of the 'analysis' package

--- backend/src/main/kotlin/controller/analysis/extraction/staticanalysis/StaticAnalysisExtractor.kt
+++ backend/src/main/kotlin/controller/analysis/extraction/staticanalysis/StaticAnalysisExtractor.kt
@@ -1,21 +1,51 @@
 package controller.analysis.extraction.staticanalysis
 
-import controller.analysis.extraction.Extractor
-import controller.analysis.extraction.Extractor.Companion.ExtractionBasePath
-import java.io.File
+import controller.analysis.extraction.AbstractExtractor
+import controller.analysis.extraction.ExtractorCompanion
+import model.graph.Attributes
+import model.graph.Edge
+import model.graph.Node
+import model.skeleton.UnitContainer
 
 
-abstract class StaticAnalysisExtractor {
-    fun buildBasePath(platformIdentifier: String, projectName: String) = "$UnarchiverBasePath/$platformIdentifier/$projectName"
+abstract class StaticAnalysisExtractor : AbstractExtractor() {
+    fun convertUnitContainerToGraph(unitContainer: UnitContainer, basePackageIdentifier: String): List<Edge> {
+        val edges = arrayListOf<Edge>()
 
-    fun cleanup(basePath: String) = File(basePath).deleteRecursively()
+        for (unit in unitContainer.units) {
+            if (unit.identifier.endsWith("Test")) continue
+            val startUnit = Node(identifier = unit.identifier, packageIdentifier = unit.packageIdentifier)
 
-    abstract fun extract(): String
+            if (unit.methods == null) continue
+            for (method in unit.methods) {
+                if (method.operations == null) continue
+                for (operation in method.operations) {
+                    if (operation.type != "call") continue
 
-    companion object : Extractor {
+                    var targetComponents = operation.target.split('.')
+                    targetComponents = targetComponents.dropLast(1)
+                    val identifier = targetComponents.last()
+                    targetComponents = targetComponents.dropLast(1)
+                    val packageIdentifier = targetComponents.joinToString(".")
+
+                    if (identifier.endsWith("Test")) continue
+                    if (!packageIdentifier.startsWith(basePackageIdentifier)) continue
+
+                    val endUnit = Node(identifier = identifier, packageIdentifier = packageIdentifier)
+
+                    if (startUnit != endUnit) {
+                        edges.add(Edge(start = startUnit, end = endUnit, attributes = Attributes(couplingScore = 1)))
+                    }
+                }
+            }
+        }
+
+        return edges
+    }
+
+    companion object : ExtractorCompanion {
         const val StaticAnalysisDirectory = "static"
         const val SkeletonDirectoryName = "skeleton"
-        const val UnarchiverBasePath = ExtractionBasePath
 
         override fun getArchiveUploadPath(): String {
             return "$UnarchiverBasePath/$StaticAnalysisDirectory"
