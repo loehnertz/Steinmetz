ae9720010b3a4bd89cd9012412b257359f60179b
Jakob Löhnertz <mail@jakob.codes>
2019-04-19 16:59:47 +0200

    Implement basic visualization of the clustered graph

--- frontend/src/components/Graph.vue
+++ frontend/src/components/Graph.vue
@@ -1,5 +1,6 @@
 <template>
     <Network
+            ref="graph"
             :nodes="graphNodes"
             :edges="graphEdges"
             :options="graphOptions"
@@ -61,57 +62,75 @@
         },
         watch: {
             graphData: function (graphData) {
-                if (graphData) this.constructGraph(this.graphData["edges"]);
+                if (graphData) {
+                    this.flushGraph();
+                    this.constructGraph(this.graphData["nodes"], this.graphData["edges"]);
+                }
             },
         },
         methods: {
-            constructGraph(relationships) {
+            flushGraph() {
+                this.graphNodeIds = new Set();
+                this.graphNodes = [];
+                this.graphEdges = [];
+            },
+            constructGraph(units, relationships) {
                 this.configureGravitation(relationships.length);
-                this.setNodes(relationships);
-                this.setEdges(relationships);
+                const clusterIds = this.setNodes(units);
+                this.setEdges(relationships, clusterIds);
             },
             configureGravitation(relationshipAmount) {
                 this.graphOptions.physics.barnesHut.gravitationalConstant = -(relationshipAmount * 100)
             },
-            setNodes(relationships) {
-                for (let relationship of relationships) {
-                    let startNode = this.buildNode(relationship.start["identifier"], relationship.start["packageIdentifier"]);
-                    let endNode = this.buildNode(relationship.end["identifier"], relationship.end["packageIdentifier"]);
+            setNodes(units) {
+                const clusterIds = units.map((unit) => {
+                    return unit["attributes"]["cluster"]
+                });
+                const clusterAmount = new Set(clusterIds).size;
 
-                    if (!this.graphNodeIds.has(startNode.id)) {
-                        this.graphNodes.push(startNode);
-                        this.graphNodeIds.add(startNode.id);
-                    }
+                for (let unit of units) {
+                    let node = this.buildUnitNode(
+                        unit["identifier"],
+                        unit["packageIdentifier"],
+                        unit["attributes"]["cluster"],
+                        clusterAmount,
+                    );
 
-                    if (!this.graphNodeIds.has(endNode.id)) {
-                        this.graphNodes.push(endNode);
-                        this.graphNodeIds.add(endNode.id);
+                    if (!this.graphNodeIds.has(node.id)) {
+                        this.graphNodes.push(node);
+                        this.graphNodeIds.add(node.id);
                     }
                 }
+
+                return clusterIds;
             },
-            setEdges(relationships) {
+            setEdges(relationships, clusterIds) {
                 for (let relationship of relationships) {
                     let startNode = relationship.start;
                     let endNode = relationship.end;
-                    let attributes = relationship.attributes;
 
-                    let edge = this.buildEdge(
-                        this.constructNodeId(startNode["identifier"], startNode["packageIdentifier"]),
-                        this.constructNodeId(endNode["identifier"], endNode["packageIdentifier"]),
-                        '',
-                        attributes["couplingScore"],
+                    let edge = this.buildUnitEdge(
+                        this.constructUnitNodeId(startNode["identifier"], startNode["packageIdentifier"]),
+                        this.constructUnitNodeId(endNode["identifier"], endNode["packageIdentifier"]),
+                        100,
                     );
 
                     this.graphEdges.push(edge);
                 }
+
+                if (this.isClustered) this.clusterGraph(clusterIds);
             },
-            constructNodeId(identifier, packageIdentifier) {
+            constructUnitNodeId(identifier, packageIdentifier) {
                 return `${packageIdentifier}.${identifier}`;
             },
-            buildNode(identifier, packageIdentifier) {
+            constructClusterNodeId(clusterId) {
+                return `${ClusterNodeKeyword}:${clusterId}`;
+            },
+            buildUnitNode(identifier, packageIdentifier, clusterId, clusterAmount) {
                 return {
-                    id: this.constructNodeId(identifier, packageIdentifier),
-                    title: identifier,
+                    id: this.constructUnitNodeId(identifier, packageIdentifier),
+                    cid: clusterId,
+                    title: this.generateNodePopup(`${packageIdentifier}.${identifier}`),
                     label: identifier,
                     borderWidth: 5,
                     color: {
@@ -120,7 +139,20 @@
                     },
                 }
             },
-            buildEdge(startNodeId, endNodeId, label, weight) {
+            buildServiceNode(clusterId) {
+                return {
+                    id: this.constructClusterNodeId(clusterId),
+                    cid: clusterId,
+                    title: this.generateNodePopup(`Service ${clusterId}`),
+                    borderWidth: 5,
+                    color: {
+                        background: 'whitesmoke',
+                        border: DefaultColor,
+                    },
+                    shape: 'hexagon',
+                }
+            },
+            buildUnitEdge(startNodeId, endNodeId, length) {
                 return {
                     from: startNodeId,
                     to: endNodeId,
@@ -138,6 +170,62 @@
                     },
                     arrowStrikethrough: false,
                 }
+            },
+            buildServiceEdge(unitNodeId, serviceNodeId) {
+                return {
+                    from: unitNodeId,
+                    to: serviceNodeId,
+                    color: {
+                        color: DefaultColor,
+                        highlight: DefaultColor,
+                    },
+                }
+            },
+            clusterGraph(clusterIds) {
+                const unit2unitEdges = this.graphEdges;
+                this.graphEdges = [];
+
+                for (let clusterId of clusterIds) {
+                    const clusterNode = this.buildServiceNode(clusterId);
+
+                    if (!this.graphNodeIds.has(clusterNode.id)) {
+                        this.graphNodes.push(clusterNode);
+                        this.graphNodeIds.add(clusterNode.id);
+                    }
+                }
+
+                for (let unitNode of this.graphNodes.filter((node) => {
+                    return !node.id.startsWith(ClusterNodeKeyword)
+                })) {
+                    const clusterEdge = this.buildServiceEdge(unitNode.id, this.constructClusterNodeId(unitNode.cid));
+                    this.graphEdges.push(clusterEdge);
+                }
+
+                for (let unitEdge of unit2unitEdges) {
+                    const fromClusterNode = this.constructClusterNodeId(this.findNodeById(unitEdge.from).cid);
+                    const toClusterNode = this.constructClusterNodeId(this.findNodeById(unitEdge.to).cid);
+
+                    if (fromClusterNode !== toClusterNode) {
+                        const clusterUnitEdge = this.buildUnitEdge(fromClusterNode, toClusterNode, 555);
+                        const existingEdgeIndex = this.graphEdges.findIndex((edge) => {
+                            return edge.from === clusterUnitEdge.from && edge.to === clusterUnitEdge.to
+                        });
+
+                        if (existingEdgeIndex === -1) {
+                            this.graphEdges.push(clusterUnitEdge);
+                        } else {
+                            this.graphEdges[existingEdgeIndex].value = this.graphEdges[existingEdgeIndex].value + 1;
+                        }
+                    }
+                }
+            },
+            findNodeById(nodeId) {
+                return this.graphNodes.find((node) => {
+                    return node.id === nodeId;
+                });
+            },
+            generateNodePopup(title) {
+                return `<span style="font-family: 'Titillium Web', sans-serif;">${title}</span>`;
             },
             getEdgeScalingFunction() {
                 return function (min, max, total, value) {
