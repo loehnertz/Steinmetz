4574f05648b610c5dddea1bbd8548d7f28ff684d
Jakob Löhnertz <mail@jakob.codes>
2019-05-02 13:49:52 +0200

    Add a separate component for the loading throbber

---
+++ frontend/src/components/Throbber.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="throbber-container">
+        <ScaleLoader :loading="isLoading"/>
+    </div>
+</template>
+
+<script>
+    import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue';
+
+    export default {
+        components: {
+            ScaleLoader,
+        },
+        props: {
+            isLoading: Boolean,
+        },
+    }
+</script>
+
+<style scoped>
+    #throbber-container {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transform: scale(1.55);
+    }
+</style>
