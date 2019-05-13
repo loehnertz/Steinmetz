931738d3db37b1efcd06531d85c4c3c025677e9a
Jakob Löhnertz <mail@jakob.codes>
2019-05-13 10:55:03 +0200

    Improve the structure of the 'Clusterer' class

--- backend/src/main/kotlin/controller/analysis/clustering/Clusterer.kt
+++ backend/src/main/kotlin/controller/analysis/clustering/Clusterer.kt
@@ -1,37 +1,19 @@
 package controller.analysis.clustering
 
+import controller.analysis.clustering.clausetnewmanmoore.ClausetNewmanMooreManager
 import controller.analysis.clustering.infomap.InfomapManager
 import controller.analysis.clustering.louvain.LouvainManager
 import controller.analysis.clustering.mcl.MclManager
 import model.graph.Graph
 
 
-class Clusterer(graph: Graph, projectName: String) {
-    private val mclManager = MclManager(graph)
-    private val infomapManager = InfomapManager(graph)
-    private val louvainManager = LouvainManager(graph, projectName)
-
+class Clusterer(private val graph: Graph, private val projectName: String) {
     fun applyClusteringAlgorithm(clusteringAlgorithm: ClusteringAlgorithm, tunableClusteringParameter: Double?): Graph {
         return when (clusteringAlgorithm) {
-            ClusteringAlgorithm.MCL -> applyMcl(tunableClusteringParameter)
-            ClusteringAlgorithm.INFOMAP -> applyInfomap(tunableClusteringParameter?.toInt())
-            ClusteringAlgorithm.LOUVAIN -> applyLouvain()
+            ClusteringAlgorithm.MCL -> MclManager(graph).apply(tunableParameter = tunableClusteringParameter)
+            ClusteringAlgorithm.INFOMAP -> InfomapManager(graph, projectName).apply(tunableParameter = tunableClusteringParameter)
+            ClusteringAlgorithm.LOUVAIN -> LouvainManager(graph, projectName).apply(tunableParameter = tunableClusteringParameter)
+            ClusteringAlgorithm.CLAUSET_NEWMAN_MOORE -> ClausetNewmanMooreManager(graph, projectName).apply(tunableParameter = tunableClusteringParameter)
         }
-    }
-
-    private fun applyMcl(clusteringInflationValue: Double?): Graph {
-        return mclManager.applyMcl(clusteringInflationValue)
-    }
-
-    private fun applyInfomap(iterationAmount: Int?): Graph {
-        return if (iterationAmount != null) {
-            infomapManager.applyInfomap(iterationAmount)
-        } else {
-            infomapManager.applyInfomap()
-        }
-    }
-
-    private fun applyLouvain(): Graph {
-        return louvainManager.applyLouvain()
     }
 }
