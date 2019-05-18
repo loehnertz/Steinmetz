cccf501580d73638f8fb518546773bd8cce6e748
Jakob Löhnertz <mail@jakob.codes>
2019-05-18 15:58:40 +0200

    Change the way a node is updated in an existing graph object. Attached attributes of the updating node will be retained.

--- backend/src/main/kotlin/model/graph/Graph.kt
+++ backend/src/main/kotlin/model/graph/Graph.kt
@@ -9,17 +9,27 @@ class Graph(val nodes: MutableSet<Node> = mutableSetOf(), val edges: MutableSet<
     }
 
     fun addOrUpdateNode(node: Node) {
+        val existingNode: Node? = nodes.firstOrNull { it == node }
         nodes.removeIf { it == node }
+
+        if (existingNode != null) {
+            if (node.attributes.footprint == null && existingNode.attributes.footprint != null) {
+                node.attributes.footprint = existingNode.attributes.footprint
+            }
+        }
+
         nodes.add(node)
     }
 
     fun addOrUpdateEdge(edge: Edge) {
         val existingEdge: Edge? = edges.find { it == edge }
+
         if (edges.removeIf { it == edge } && existingEdge != null) {
             edges.add(mergeEqualEdges(existingEdge, edge))
         } else {
             edges.add(edge)
         }
+
         inferNodesOutOfEdges()
     }
 
