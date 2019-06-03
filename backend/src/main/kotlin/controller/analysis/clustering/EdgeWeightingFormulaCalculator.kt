01e01e72f24839af70f1440cea1a30ca1e27523f
Jakob Löhnertz <mail@jakob.codes>
2019-06-03 17:19:31 +0200

    Add the semantic coupling score to the edge weighting

--- backend/src/main/kotlin/controller/analysis/clustering/EdgeWeightingFormulaCalculator.kt
+++ backend/src/main/kotlin/controller/analysis/clustering/EdgeWeightingFormulaCalculator.kt
@@ -6,7 +6,9 @@ import model.graph.EdgeAttributes
 object EdgeWeightingFormulaCalculator {
     fun applyFormula(edgeAttributes: EdgeAttributes): Int {
         val weightedDynamicCouplingScore: Double = edgeAttributes.dynamicCouplingScore.toDouble()
+        val weightedSemanticCouplingScore: Double = Math.ceil(edgeAttributes.semanticCouplingScore.toDouble() / 10)
         val weightedLogicalCouplingScore: Double = Math.ceil(edgeAttributes.logicalCouplingScore.toDouble() / 10)
-        return (weightedDynamicCouplingScore + weightedLogicalCouplingScore).toInt() + 1
+
+        return (weightedDynamicCouplingScore + weightedSemanticCouplingScore + weightedLogicalCouplingScore).toInt() + 1
     }
 }
--- backend/src/main/kotlin/model/metrics/InputQuality.kt
+++ backend/src/main/kotlin/model/metrics/InputQuality.kt
@@ -2,5 +2,7 @@ package model.metrics
 
 
 data class InputQuality(
-        val dynamicAnalysis: Int
+        val dynamicAnalysis: Int,
+        val semanticAnalysis: Int,
+        val logicalAnalysis: Int
 )
