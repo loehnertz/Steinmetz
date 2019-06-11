344027280d39ab66687295dfbf91f91c9a4d0d38
Jakob Löhnertz <mail@jakob.codes>
2019-06-11 16:00:51 +0200

    Cleanup more temporary files when analyzing 'JFR' files

--- backend/src/main/kotlin/controller/analysis/extraction/coupling/dynamic/platforms/jvm/JfrRecordingAnalyzer.kt
+++ backend/src/main/kotlin/controller/analysis/extraction/coupling/dynamic/platforms/jvm/JfrRecordingAnalyzer.kt
@@ -20,8 +20,8 @@ class JfrRecordingAnalyzer(projectName: String, private val basePackageIdentifie
 
     override fun extract(): Graph {
         val dynamicAnalysisGraph: Graph = analyzeRecording()
-        cleanup(dynamicAnalysisBasePath)
-        return dynamicAnalysisGraph
+        cleanup(basePath, dynamicAnalysisBasePath)
+        return weightDynamicCouplingScores(dynamicAnalysisGraph)
     }
 
     private fun analyzeRecording(): Graph {
