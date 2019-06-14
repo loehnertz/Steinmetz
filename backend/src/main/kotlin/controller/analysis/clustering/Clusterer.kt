14a5eb57703cb14e4629bb2f57a59bfcc160aae9
Jakob Löhnertz <mail@jakob.codes>
2019-06-14 09:53:04 +0200

    Add a manager for the new algorithm

--- backend/src/main/kotlin/controller/analysis/clustering/Clusterer.kt
+++ backend/src/main/kotlin/controller/analysis/clustering/Clusterer.kt
@@ -1,5 +1,6 @@
 package controller.analysis.clustering
 
+import controller.analysis.clustering.chinesewhispers.ChineseWhispersManager
 import controller.analysis.clustering.clausetnewmanmoore.ClausetNewmanMooreManager
 import controller.analysis.clustering.infomap.InfomapManager
 import controller.analysis.clustering.louvain.LouvainManager
@@ -21,6 +22,7 @@ class Clusterer(private val graph: Graph, private val projectName: String, priva
             ClusteringAlgorithm.LOUVAIN -> LouvainManager(graph, projectName).apply(tunableParameter = tunableClusteringParameter)
             ClusteringAlgorithm.CLAUSET_NEWMAN_MOORE -> ClausetNewmanMooreManager(graph, projectName).apply(tunableParameter = tunableClusteringParameter)
             ClusteringAlgorithm.WALKTRAP -> WalktrapManager(graph, projectName).apply(tunableParameter = tunableClusteringParameter)
+            ClusteringAlgorithm.CHINESE_WHISPERS -> ChineseWhispersManager(graph).apply(tunableParameter = tunableClusteringParameter)
         }
     }
 }
