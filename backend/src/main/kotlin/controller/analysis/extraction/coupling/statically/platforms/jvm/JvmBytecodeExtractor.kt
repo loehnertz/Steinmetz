779b58beb87a0b54766406b9bf16e8079f7eeb26
Jakob Löhnertz <mail@jakob.codes>
2019-06-17 18:00:49 +0200

    Got rid of 'jpeek' and introduced 'javacg' instead to statically analyze '.jar' files

--- backend/src/main/kotlin/controller/analysis/extraction/coupling/statically/platforms/jvm/JvmBytecodeExtractor.kt
+++ backend/src/main/kotlin/controller/analysis/extraction/coupling/statically/platforms/jvm/JvmBytecodeExtractor.kt
@@ -3,22 +3,16 @@ package controller.analysis.extraction.coupling.statically.platforms.jvm
 import controller.analysis.extraction.Platform
 import controller.analysis.extraction.coupling.statically.StaticAnalysisExtractor
 import controller.analysis.extraction.coupling.statically.StaticAnalysisExtractorCompanion
-import controller.analysis.extraction.graph.UnitContainerExtractor
-import model.graph.Graph
-import model.graph.Node
+import gr.gousiosg.javacg.stat.ClassVisitor
+import gr.gousiosg.javacg.stat.JCallGraph
+import model.graph.*
 import model.graph.Unit
-import model.graph.UnitFootprint
-import model.skeleton.UnitContainer
 import utility.ArchiveExtractor
 import java.io.File
-import javax.xml.transform.Transformer
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.stream.StreamResult
-import javax.xml.transform.stream.StreamSource
+import java.util.regex.Pattern
 
 
-// TODO: Refactor this into something less hacky
-class JvmBytecodeExtractor(private val projectName: String, private val basePackageIdentifier: String, private val archive: File) : StaticAnalysisExtractor() {
+class JvmBytecodeExtractor(projectName: String, private val basePackageIdentifier: String, private val archive: File) : StaticAnalysisExtractor() {
     private val basePath: String = buildBasePath(platformIdentifier, projectName)
     private val staticAnalysisBasePath = "$basePath/$StaticAnalysisDirectory"
     private val unarchiver = ArchiveExtractor(".$classFileExtension", "$staticAnalysisBasePath/$unarchiverPath")
@@ -26,29 +20,29 @@ class JvmBytecodeExtractor(private val projectName: String, private val basePack
     override fun extract(): Graph {
         unarchiver.unpackAnalysisArchive(archive)
 
-        Runtime.getRuntime().exec("java -jar backend/src/main/resources/jpeek.jar --sources $staticAnalysisBasePath/$unarchiverPath/${buildBasePackagePath()} --target $staticAnalysisBasePath/$SkeletonDirectoryName/jpeek --overwrite").waitFor()
+        val jcallGraph: JCallGraph = JCallGraph(listOf(archive), listOf(Pattern.compile("$basePackageIdentifier.*"))).also { it.analyze() }
+        val invokations: List<Pair<String, String>> = jcallGraph.classVisitors.flatMap { it.classInvokations }.map { it.split(' ') }.map { Pair(it.first().replace(ClassVisitor.PREFIX, ""), it.last()) }
 
-        val convertedSkeletonXml: String = convertSkeletonXml(File("$staticAnalysisBasePath/$SkeletonDirectoryName/jpeek/skeleton.xml")).readText()
-        val unitContainer: UnitContainer = UnitContainerExtractor.extract(convertedSkeletonXml)
-        val graph: Graph = convertUnitContainerToGraph(unitContainer, basePackageIdentifier)
+        val graph: Graph = convertInvokationsPairsToGraph(invokations)
         graph.nodes.map { attachUnitFootprint(it) }.forEach { graph.addOrUpdateNode(it) }
+
         cleanup(staticAnalysisBasePath)
 
         return graph
     }
 
-    private fun convertSkeletonXml(skeletonFile: File): File {
-        val xmlInput = StreamSource(skeletonFile)
-        val xsl = StreamSource(File("backend/src/main/resources/jpeek-skeleton.xsl"))
-        val outputFile = File("$staticAnalysisBasePath/$SkeletonDirectoryName/$projectName-processed.xml")
-        outputFile.parentFile.mkdirs()
-        outputFile.createNewFile()
-        val xmlOutput = StreamResult(outputFile)
+    private fun convertInvokationsPairsToGraph(invokations: List<Pair<String, String>>): Graph {
+        val graph = Graph()
 
-        val transformer: Transformer = TransformerFactory.newInstance().newTransformer(xsl)
-        transformer.transform(xmlInput, xmlOutput)
+        for (invokation: Pair<String, String> in invokations) {
+            val startUnit = Unit(identifier = invokation.first.substringAfterLast('.'), packageIdentifier = invokation.first.substringBeforeLast('.'))
+            val endUnit = Unit(identifier = invokation.second.substringAfterLast('.'), packageIdentifier = invokation.second.substringBeforeLast('.'))
+            val edge = Edge(start = startUnit, end = endUnit, attributes = EdgeAttributes())
 
-        return outputFile
+            graph.addOrUpdateEdge(edge)
+        }
+
+        return graph
     }
 
     private fun attachUnitFootprint(node: Node): Node {
@@ -60,10 +54,6 @@ class JvmBytecodeExtractor(private val projectName: String, private val basePack
 
     private fun calculateUnitFootprint(unit: Unit): Long {
         return File("$staticAnalysisBasePath/$unarchiverPath/${unit.packageIdentifier.replace('.', '/')}/${unit.identifier}.$classFileExtension").length()
-    }
-
-    private fun buildBasePackagePath(): String {
-        return basePackageIdentifier.replace('.', '/')
     }
 
     companion object : StaticAnalysisExtractorCompanion {
