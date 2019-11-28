cf025f79f0bed327af4601b73baed0bcc01d58fc
Jakob Löhnertz <mail@jakob.codes>
2019-11-28 17:42:52 +0100

    Leverage the new '.mapConcurrently()' extension function in two places

--- backend/src/main/kotlin/controller/analysis/clustering/mcl/MclManager.kt
+++ backend/src/main/kotlin/controller/analysis/clustering/mcl/MclManager.kt
@@ -2,31 +2,27 @@ package controller.analysis.clustering.mcl
 
 import controller.analysis.clustering.ClusteringAlgorithmManager
 import controller.analysis.metrics.clustering.ClusteringQualityAnalyzer
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import model.graph.Graph
 import model.graph.Node
 import model.graph.NodeAttributes
 import model.graph.Unit
 import model.metrics.ClusteringQuality
+import utility.mapConcurrently
 import java.math.RoundingMode
 import kotlin.reflect.KProperty1
 
 
 class MclManager(private val graph: Graph, private val chosenClusteringMetric: KProperty1<ClusteringQuality, *>) : ClusteringAlgorithmManager {
     override fun apply(iterations: Int): Graph {
-        val clusteringInflationValues: List<Double> = buildClusteringInflationValuesList()
-
-        val input: String = convertGraphToInput()
-
-        val deferredExecutions: ArrayList<Deferred<String>> = arrayListOf()
-        for (clusteringInflationValue: Double in clusteringInflationValues) deferredExecutions.add(GlobalScope.async { MclExecutor(input, clusteringInflationValue).execute() })
-
         return runBlocking {
-            val clusteredGraphs: List<Graph> = deferredExecutions.map { it.await() }.map { convertOutputToGraph(it, Graph(nodes = graph.nodes.map { node -> node.copy() }.toMutableSet(), edges = graph.edges)) }
-            return@runBlocking clusteredGraphs.maxBy { (chosenClusteringMetric.get(ClusteringQualityAnalyzer(it).calculateClusteringQualityMetrics()) as Number).toDouble() }!!
+            val clusteringInflationValues: List<Double> = buildClusteringInflationValuesList()
+            val input: String = convertGraphToInput()
+            val clusteredGraphs: List<Graph> = clusteringInflationValues
+                .mapConcurrently { clusteringInflationValue -> MclExecutor(input, clusteringInflationValue).execute() }
+                .map { convertOutputToGraph(it, Graph(nodes = graph.nodes.map { node -> node.copy() }.toMutableSet(), edges = graph.edges)) }
+            return@runBlocking clusteredGraphs
+                .maxBy { (chosenClusteringMetric.get(ClusteringQualityAnalyzer(it).calculateClusteringQualityMetrics()) as Number).toDouble() }!!
         }
     }
 
