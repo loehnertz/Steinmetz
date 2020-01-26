aa5b5673c9999528af801c1d224de3aa8d282fda
Jakob Löhnertz <mail@jakob.codes>
2020-01-26 13:26:42 +0100

    Adapt the 'LouvainManager' to use the new abstract 'Neo4j' class

--- backend/src/main/kotlin/controller/analysis/clustering/louvain/LouvainManager.kt
+++ backend/src/main/kotlin/controller/analysis/clustering/louvain/LouvainManager.kt
@@ -1,87 +1,28 @@
 package controller.analysis.clustering.louvain
 
-import controller.analysis.clustering.ClusteringAlgorithmManager
-import model.graph.*
-import model.graph.Unit
+import controller.analysis.clustering.AbstractNeo4jAlgorithmManager
+import model.graph.EdgeAttributes
+import model.graph.Graph
 import model.neo4j.relationship.CallsRelation
 import org.neo4j.ogm.model.Result
 import utility.Neo4jConnector
 
 
-class LouvainManager(private val graph: Graph, private val projectName: String) : ClusteringAlgorithmManager {
-    override fun apply(iterations: Int): Graph {
-        addNecessaryLabels()
-        val result: Result = executeGraphAlgorithm()
-        removeNecessaryLabels()
+class LouvainManager(graph: Graph, projectName: String) : AbstractNeo4jAlgorithmManager(graph, projectName) {
+    override val nodeLabel: String = LouvainNodeLabel
 
-        for (resultMap: MutableMap<String, Any> in result) {
-            val unitNode: model.neo4j.node.Unit = (resultMap[UnitIdentifier] as model.neo4j.node.Unit)
-            val cluster: Int = (resultMap[ClusterIdentifer] as Long).toInt() + 1  // It otherwise starts zero-indexed, which no other clustering algorithm does
-            val unit = Unit(identifier = unitNode.identifier, packageIdentifier = unitNode.packageIdentifier)
-            val node = Node(unit = unit, attributes = NodeAttributes(cluster = cluster))
-            graph.addOrUpdateNode(node)
-        }
+    override val clusterIdentifer: String = ClusterIdentifer
 
-        return graph
-    }
-
-    private fun executeGraphAlgorithm(): Result {
+    override fun executeGraphAlgorithm(iterations: Int): Result {
         return Neo4jConnector.executeCypher(
-            "CALL algo.louvain.stream('$LouvainNodeLabel', '$CallsRelation', {weightProperty:'${EdgeAttributes::couplingScore.name}'})\n" +
-            "YIELD nodeId, $ClusterIdentifer\n" +
-            "RETURN algo.asNode(nodeId) AS $UnitIdentifier, $ClusterIdentifer"
-        )
-    }
-
-    private fun addNecessaryLabels() {
-        for (edge in graph.edges) {
-            Neo4jConnector.executeCypher(
-                "MATCH " +
-                "(s:Unit {" +
-                "${model.neo4j.node.Unit::identifier.name}:'${edge.start.identifier}', " +
-                "${model.neo4j.node.Unit::packageIdentifier.name}:'${edge.start.packageIdentifier}'" +
-                "})" +
-                "-[r]->" +
-                "(e:Unit {" +
-                "${model.neo4j.node.Unit::identifier.name}:'${edge.end.identifier}', " +
-                "${model.neo4j.node.Unit::packageIdentifier.name}:'${edge.end.packageIdentifier}'" +
-                "})\n" +
-                "SET r.${edge.attributes::couplingScore.name} = ${edge.attributes.couplingScore}\n"
-            )
-        }
-
-        Neo4jConnector.executeCypher(
-            "MATCH (u:Unit {${model.neo4j.node.Unit::projectName.name}:'$projectName'})\n" +
-            "SET u:$LouvainNodeLabel\n"
-        )
-    }
-
-    private fun removeNecessaryLabels() {
-        for (edge in graph.edges) {
-            Neo4jConnector.executeCypher(
-                "MATCH " +
-                "(s:Unit {" +
-                "${model.neo4j.node.Unit::identifier.name}:'${edge.start.identifier}', " +
-                "${model.neo4j.node.Unit::packageIdentifier.name}:'${edge.start.packageIdentifier}'" +
-                "})" +
-                "-[r]->" +
-                "(e:Unit {" +
-                "${model.neo4j.node.Unit::identifier.name}:'${edge.end.identifier}', " +
-                "${model.neo4j.node.Unit::packageIdentifier.name}:'${edge.end.packageIdentifier}'" +
-                "})\n" +
-                "REMOVE r.${edge.attributes::couplingScore.name}\n"
-            )
-        }
-
-        Neo4jConnector.executeCypher(
-            "MATCH (u:$LouvainNodeLabel {${model.neo4j.node.Unit::projectName.name}:'$projectName'})\n" +
-            "REMOVE u:$LouvainNodeLabel\n"
+            "CALL algo.louvain.stream('$nodeLabel', '$CallsRelation', {direction: 'BOTH', weightProperty: '${EdgeAttributes::couplingScore.name}'})\n" +
+            "YIELD nodeId, $clusterIdentifer\n" +
+            "RETURN algo.asNode(nodeId) AS $UnitIdentifier, $clusterIdentifer"
         )
     }
 
     companion object {
         private const val LouvainNodeLabel = "louvain"
         private const val ClusterIdentifer = "community"
-        private const val UnitIdentifier = "unit"
     }
 }
